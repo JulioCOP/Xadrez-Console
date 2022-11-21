86387ca44315c610a47984ce26de2c46e4936496
Julio Oliveira <juliocesar.jc585@gmail.com>
2022-11-21 07:31:15 -0300

    Criada a classe de posicao no tabuleiro

---
+++ Xadrez-Console/Tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; } 
+
+        public Posicao()
+        {
+
+        }
+        public Posicao (int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString() // Retornar um valor como string no programa principal
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
