79dc13d99b15d9cac7d5053195384e735be8a41b
Julio Oliveira <juliocesar.jc585@gmail.com>
2022-11-23 07:52:17 -0300

    Criando a classe PosiçãoXadrez

---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Console.xadrez
+{
+    internal class PosicaoXadrez
+    {
+    }
+}
